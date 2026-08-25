1a042c507bd860079fc74efad547f4632f9c89ae
HRDLICKA Jan (renexter) <jan.hrdlicka-extern@renault.cz>
2026-08-25 18:31:13 +0200

    small changes

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const INTAKE_FORM_URL =
-  'https://docs.google.com/forms/d/e/1FAIpQLSeDqxPWh4zs07Sde5k42Yxts7bVUL1XLElxWKeVzfUV-HkjQQ/viewform?usp=header';
+  'https://docs.google.com/forms/d/e/1FAIpQLSeDqxPWh4zs07Sde5k42Yxts7bVUL1XLElxWKeVzfUV-HkjQQ/viewform';
